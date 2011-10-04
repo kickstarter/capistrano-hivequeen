8664016816ef4cd916d7c6274b9d9187146f8348
Aaron Suggs <aaron@ktheory.com>
2011-10-04 18:25:59 -0400

    Use Excon for sane HTTPS & Basic auth

--- lib/capistrano/hivequeen/server.rb
+++ lib/capistrano/hivequeen/server.rb
@@ -1,17 +1,18 @@
 # HTTP Client for Hive Queen environment configuration
-require 'open-uri'
 require 'json'
 require 'fileutils'
+require 'excon'
+require 'base64'
 
 # Special cases:
 # - environment not found
 # - environment not ready: fail
 class HiveQueen
   class << self
-    attr_accessor :endpoint, :logger, :project
+    attr_accessor :endpoint, :logger, :project, :username, :password
 
     def project_data
-      @project_data ||= fetch("/projects/#{project}")
+      @project_data ||= fetch("/projects/#{project}.json")
     end
 
     def environments
@@ -29,14 +30,41 @@ class HiveQueen
     def roles(env_id)
       env_id = env_id.to_sym
       @roles ||= {}
-      @roles[env_id] ||= fetch("/environments/#{env_id}")
+      @roles[env_id] ||= fetch("/environments/#{env_id}.json")
+    end
+
+    # Try to load credentials, or read them from ~/.hivequeen
+    def set_credentials!
+      @username, @password = File.read(credential_path).chomp.split(':')
+      raise "Could not read credentials from #{credential_path}. Try removing the file."
+    rescue Errno::ENOENT
+      puts "Enter your username:"
+      @username = gets.chomp
+      puts "Enter your password:"
+      @password = gets.chomp
+      puts "Saving credentials to #{credential_path}"
+      File.open(credential_path, 'w', 0600){|f| f << [username, password] * ':'}
+      retry
     end
 
     protected
+    def credential_path
+      File.join(ENV['HOME'], '.hivequeen')
+    end
+
+    def connection
+      @connection ||= Excon.new(endpoint)
+    end
+
+    def auth_header
+      value = Base64.encode64([username, password] * ':').chomp
+      { 'Authorization' => "Basic #{value}" }
+    end
+
     def fetch(path)
-      url = endpoint + path + '.json'
-      logger.trace "Fetching #{url}"
-      JSON.parse(open(url).read)
+      logger.trace "Fetching #{endpoint}/#{path}"
+      response = connection.get(:path => path, :headers => auth_header)
+      JSON.parse(response.body)
     end
 
   end
